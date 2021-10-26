e7cea78701273631f912cffe52f42d7d3daad4cd
pratham7876 <93199411+pratham7876@users.noreply.github.com>
2021-10-26 18:21:13 +0530

    Create speech.py

---
+++ speech.py
@@ -0,0 +1,21 @@
+def speak(audio):
+    engine.say(audio)
+    print(f" : {audio}")
+    engine.runAndWait()
+# convert voice to text
+def takecommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listneing....")
+        r.pause_threshold=1  # agar bolte bolte ruke to listening bnd na hoje
+        audio = r.listen(source,timeout=4,phrase_time_limit=5)
+
+    try:
+        print("Recognizing...")
+        text=r.recognize_google(audio, language='en-in')
+        print(f"You said:{text}")
+    except Exception as e:
+        #speak("say that again please...")
+        return "none"
+    text=text.lower()
+    return text
